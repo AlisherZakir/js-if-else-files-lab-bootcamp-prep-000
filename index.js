178498c7e642f249cea74a36877549f6bfb5dfeb
Alisher Zakir <a3zakir@gmail.com>
2019-04-18 00:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 
@@ -9,6 +9,6 @@ module.exports = { name, height
 if (name === "Susan") {
   console.log("The name is correct")
 } else {
-  console.log("Expected to be Susan")
+  console.log("Expected the name to be Susan")
 }
 
